55c75f4838d1e1f7b72bc8aac758200a614667cb
p0tat0627 <maksudulhaque@iut-dhaka.edu>
2023-10-20 19:55:57 +0600

    104. Maximum Depth of Binary Tree

---
+++ max_depth_of_btree.cpp
@@ -0,0 +1,31 @@
+// 104. Maximum Depth of Binary Tree: https://leetcode.com/problems/maximum-depth-of-binary-tree/
+// binary tree, recursion, height
+
+// approach
+// watch this: https://youtu.be/eD3tmO66aBA
+// if totalwindowsize - max_freq of a char in that window = less of equal to given k, then okay
+// otherwise slide left pointer to right and decrease frequency of left pointer
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr) return 0;
+        
+        int left_subtree = maxDepth(root->left);
+        int right_subtree = maxDepth(root->right);
+        
+        return 1+max(left_subtree, right_subtree);
+    }
+};
